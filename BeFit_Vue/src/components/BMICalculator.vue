5d17763f9e0d7f2fda16344e49bcfd4c5ce4c508
Bureeeek <burakkocbk653@gmail.com>
2023-12-13 11:26:37 +0100

    1
    Anfang

---
+++ BeFit_Vue/src/components/BMICalculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>BMI Calculator</h2>
+    <!-- Your BMI Calculator UI and logic here -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Component logic goes here
+};
+</script>
+
+<style scoped>
+/* Component styles go here */
+</style>
